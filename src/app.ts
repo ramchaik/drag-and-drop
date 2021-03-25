f27a85cea2a91cddbd6a884c0949858eaa50475f
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2021-03-25 10:33:55 +0530

    feat: fetching user input

--- src/app.ts
+++ src/app.ts
@@ -52,10 +52,31 @@ class ProjectInput {
     this.element.addEventListener("submit", this.submitHandler);
   }
 
+  private gatherUserInput(): [string, string, number] | void {
+    const enteredTitle = this.titleInputElement.value;
+    const enteredDescription = this.descriptionInputElement.value;
+    const enteredPeople = this.peopleInputElement.value;
+
+    if (
+      enteredTitle.trim().length === 0 ||
+      enteredDescription.trim().length === 0 ||
+      enteredPeople.trim().length === 0
+    ) {
+      alert("Invalid input, Please try again!");
+      return;
+    }
+
+    return [enteredTitle, enteredDescription, +enteredPeople];
+  }
+
   @autobind
   private submitHandler(event: Event) {
     event.preventDefault();
-    console.log(this.titleInputElement.value);
+    const userInput = this.gatherUserInput();
+    if (Array.isArray(userInput)) {
+      const [title, description, people] = userInput;
+      console.log(title, description, people);
+    }
   }
 
   private attach() {
