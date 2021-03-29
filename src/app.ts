d16c06503feb5552ae2dd869e01477ea7ce78b61
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2021-03-29 19:34:42 +0530

    feat: rendering project items with a class

--- src/app.ts
+++ src/app.ts
@@ -159,6 +159,30 @@ abstract class Component<T extends HTMLElement, U extends HTMLElement> {
   abstract renderContent(): void;
 }
 
+// * ProjectItem class
+class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> {
+  private project: Project;
+
+  constructor(hostId: string, project: Project) {
+    super("single-project", hostId, false, project.id);
+    this.project = project;
+    console.log('project :>> ', project);
+
+    this.configure();
+    this.renderContent();
+  }
+
+  configure() {}
+
+  renderContent() {
+    this.element.querySelector("h2")!.textContent = this.project.title;
+    this.element.querySelector(
+      "h3"
+    )!.textContent = this.project.people.toString();
+    this.element.querySelector("p")!.textContent = this.project.description;
+  }
+}
+
 // * ProjectList class
 class ProjectList extends Component<HTMLDivElement, HTMLElement> {
   assignedProjects: Project[];
@@ -195,11 +219,10 @@ class ProjectList extends Component<HTMLDivElement, HTMLElement> {
     )! as HTMLUListElement;
 
     listEl.innerHTML = "";
-    this.assignedProjects.forEach((prjItem) => {
-      const listItem = document.createElement("li");
-      listItem.textContent = prjItem.title;
-      listEl.appendChild(listItem);
-    });
+    this.assignedProjects.forEach(
+      (prjItem) =>
+        new ProjectItem(this.element.querySelector('ul')!.id, prjItem)
+    );
   }
 }
 
