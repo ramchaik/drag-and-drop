90a7f491f48f3753b1ca654d03884b0bc07f3052
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2021-03-29 20:22:11 +0530

    feat: add droppable area

--- src/app.ts
+++ src/app.ts
@@ -193,7 +193,8 @@ class ProjectItem
 
   @autobind
   dragStartHandler(event: DragEvent) {
-    console.log(event);
+    event.dataTransfer!.setData("text/plain", this.project.id);
+    event.dataTransfer!.effectAllowed = "move";
   }
 
   @autobind
@@ -229,12 +230,17 @@ class ProjectList
 
   @autobind
   dragOverHandler(event: DragEvent) {
-    const listEl = this.element.querySelector("ul")!;
-    listEl.classList.add("droppable");
+    if (event.dataTransfer && event.dataTransfer.types[0] === "text/plain") {
+      event.preventDefault();
+      const listEl = this.element.querySelector("ul")!;
+      listEl.classList.add("droppable");
+    }
   }
 
   @autobind
-  dropHandler(event: DragEvent) {}
+  dropHandler(event: DragEvent) {
+    console.log(event.dataTransfer!.getData('text/plain'));
+  }
 
   @autobind
   dragLeaveHandler(event: DragEvent) {
